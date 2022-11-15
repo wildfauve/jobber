7a623b4c53b30f8ca32633a00fc4c9f5fd460420
Col Perks <wild.fauve@gmail.com>
2022-11-16 11:46:49 +1300

    fix db file systme path

--- jobber/template/test/shared/config_for_testing.py
+++ jobber/template/test/shared/config_for_testing.py
@@ -22,8 +22,8 @@ COSMOS_DB_NAME = '<cosmos_db_name>'
 COSMOS_CONTAINER_NAME = '<cosmos_container_name>'
 SECRETS_SCOPE = '<secret_scope>'  # delete if you want the default: <domain>.<service>.<env>
 
-DB_FILE_SYSTEM_PATH_ROOT = f\"domains/{DOMAIN_NAME}/data_products/{DATA_PRODUCT_NAME}\"
-CHECKPOINT_OVERRIDE = f\"spark-warehouse/{DB_FILE_SYSTEM_PATH_ROOT}\"
+DB_FILE_SYSTEM_PATH_ROOT = 'domains/{domain}/data_products/{dataproduct}'
+CHECKPOINT_OVERRIDE = 'spark-warehouse/domains/{domain}/data_products/{dataproduct}'
 
 
 config = {{"env": env.Env().env}}
